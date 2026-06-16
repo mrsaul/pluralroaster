d11caf275d3d8ee55b98ea2ad8840581fff92bf1
mrsaul <mrsaul@users.noreply.github.com>
2026-06-16 18:10:42 +0200

    feat(db): rename pricing tiers to 30 kilos / 100 kilos / +100 kilos

---
+++ supabase/migrations/20260616300000_rename_pricing_tiers.sql
@@ -0,0 +1,3 @@
+UPDATE public.pricing_tiers SET name = '30 kilos'   WHERE name = 'Bronze';
+UPDATE public.pricing_tiers SET name = '100 kilos'  WHERE name = 'Argent';
+UPDATE public.pricing_tiers SET name = '+100 kilos' WHERE name = 'Or';
